2a400eb981f9ffd38ed28b421f484fbda1840eff
mukesh singh <mukesh.singh@zeuslearning.com>
2017-02-27 20:03:18 +0530

    recorder : created new file recorder-with-encoder.js

---
+++ audio-recorder/js/recorder-with-encoder.js
@@ -0,0 +1,105 @@
+(function(){
+    var AudioRecorder = function(){
+
+        //configuration
+        var config = {};
+        this.config.numberOfChannels = 1;
+        this.config.originalSampleRate = 44100;
+        this.config.encoderSampleRate = 44100;
+        this.config.maxBufferPerPage = 40;
+        this.config.encoderApplication = 2049;
+        this.config.encoderFrameSize = 20;
+        this.config.bufferLength = 4096;
+        this.config.resampleQuality = 3;
+        this.config.encoderPath = 'encoderWorker.min.js';
+
+         //variable initialization
+        this.recordAudioButton = document.getElementById('record'), this.stopAudioButton = document.getElementById('stop');
+
+        this.AudioContext = null, this.context = null, this.microphoneInput = null, this.audioProcessorNode = null, this.source = null;
+        this.outputData = new Array(), this.timeDifference = new Array(), this.lastOutputDataIndex = 0;
+        this.encoderWorker = null;
+    };
+
+
+    //audio recorder
+    AudioRecorder.prototype.record = function (stream) {
+
+        this.AudioContext = window.AudioContext || window.webkitAudioContext;
+        this.context = new AudioContext();
+
+        this.microphoneInput = this.context.createMediaStreamSource(stream);
+        this.audioProcessorNode = this.context.createScriptProcessor(this.config.bufferLength, this.config.numberOfChannels, this.config.numberOfChannels);
+
+        this.microphoneInput.connect(this.audioProcessorNode); //microphone -> audioprocessing
+        this.audioProcessorNode.connect(this.context.destination); //audioprocessing -> destination 
+
+        var oldThis = this;
+
+        //process audio data while recording 
+        this.audioProcessorNode.onaudioprocess = function (e) {
+
+            var chunkBuffer = new Array(oldThis.config.numberOfChannels);
+
+            for (var channelIndex = 0; channelIndex < oldThis.config.numberOfChannels; ++channelIndex) {
+
+                chunkBuffer[channelIndex]  = [];
+                var inputBuffer = e.inputBuffer.getChannelData(channelIndex);
+                var outputBuffer = e.outputBuffer.getChannelData(channelIndex);
+
+                for (var i = 0; i < inputBuffer.length; i++) {
+                    outputBuffer[i] = inputBuffer[i];
+                    chunkBuffer[channelIndex][i] = inputBuffer[i]; 
+                }               
+            }
+
+            //encode current packet
+            oldThis.encoderWorker.postMessage({
+                command : 'encode',
+                buffers : chunkBuffer
+            });
+
+            oldThis.addChunk(chunkBuffer, Date.now());
+        }
+
+    };
+
+    //send or save data
+    
+
+    AudioRecorder.prototype.startRecording = function () {
+        //set encoder worker
+        this.encoderWorker = new Worker(this.config.encoderPath);
+
+        //set buttons
+        this.recordAudioButton.disabled = true;
+        this.stopAudioButton.disabled = false;
+        
+        //get stream from microphone
+        navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(recorder.record.bind(this)).catch(recorder.error);
+    };
+
+    
+    AudioRecorder.prototype.stopRecording = function () {
+        
+        //close encoding Worker
+        this.encoderWorker.postMessage({
+            command : 'done'
+        });
+
+        //reset buttons
+        this.stopAudioButton.disabled = true;
+        this.recordAudioButton.disabled = false;
+        
+        //disconnect media stream
+        this.microphoneInput.disconnect(this.audioProcessorNode);
+        this.audioProcessorNode.disconnect(this.context.destination);
+
+        
+        //this.printFinalOutput();
+        //this.downloadJson();
+    };
+
+
+
+})();
